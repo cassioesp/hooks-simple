04c6440c701406972026f0ca3c6c81ca39a1fe39
Cássio Espindola <cassioafonso@gmail.com>
2020-04-12 15:33:39 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    state = {
+        resource: 'posts'
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
+                    <button onClick={() => this.setState({resource: 'todos'})}>Todos</button>
+                </div>
+                {this.state.resource}
+            </div>
+        )
+    }
+
 }
 
 export default App;
